6c75e4a251d3b12ee5257adf0b8054c0cd344bc3
Puth54 <151760970+Puth54@users.noreply.github.com>
2026-03-07 17:09:22 +0700

    Update Product.java

--- src/Product/Product.java
+++ src/Product/Product.java
@@ -1,6 +1,8 @@
 package Product;
 
-public abstract class Product {
+import java.io.Serializable;
+
+public abstract class Product implements Serializable {
 
     // attribute
     private String productId;
